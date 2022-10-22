987936689f40fa290aefd4c4ff2757349000b226
Jonas Wolf <jonas.wolf@jonaswolf.org>
2022-10-22 14:06:26 +0200

    Improve.

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell};
+use std::cell::RefCell;
 
 // #[derive(Debug, Default)]
 pub struct File {
@@ -29,9 +29,10 @@ impl<'a> Iterator for &EntryIter<'a> {
         let mut iter_stack = self.iter_stack.borrow_mut();
         let mut path = self.path.borrow_mut();
         loop {
-            if let Some(Entry::File(_)) = path.last() { // Pop last element from child folder if a file
+            if let Some(Entry::File(_)) = path.last() {
+                // Pop last element from child folder if a file
                 path.pop();
-            } 
+            }
             match iter_stack.last_mut().and_then(|x| x.next()) {
                 Some(entry) => {
                     if let Entry::Folder(folder) = entry {
@@ -64,6 +65,17 @@ impl<'a> EntryIter<'a> {
             .collect::<Vec<String>>()
             .join(separator)
     }
+
+    pub fn find_file(&self, filename: &str, separator: &str) -> Option<String> {
+        for iter in self {
+            if let Entry::File(f) = iter {
+                if f.name == filename {
+                    return Some(self.get_path(separator));
+                }
+            }
+        }
+        None
+    }
 }
 
 pub struct Content {
@@ -84,6 +96,45 @@ mod test {
     use super::*;
     #[test]
     fn simple_test() {
+        let content = get_content();
+        let mut output = vec![];
+        let ci = content.iter();
+        for x in &ci {
+            output.push(format!("{}", ci.get_path("/")));
+            if let Entry::File(f) = x {
+                if f.name == "b.txt" {
+                    output.push(format!("b.txt found"));
+                }
+            }
+        }
+        let expected = vec![
+            "a",
+            "a/c.txt",
+            "a/d",
+            "a/d/e.txt",
+            "a/b.txt",
+            "b.txt found",
+            "a/i",
+            "a/f.txt",
+            "g.txt",
+            "h",
+        ];
+        assert_eq!(output.len(), expected.len());
+        for (l, r) in output.iter().zip(expected.iter()) {
+            assert_eq!(l, r);
+        }
+    }
+
+    #[test]
+    fn find_test() {
+        assert_eq!(
+            get_content().iter().find_file("b.txt", "/"),
+            Some("a/b.txt".to_owned())
+        );
+        assert_eq!(get_content().iter().find_file("h", "/"), None);
+    }
+
+    fn get_content() -> Content {
         let content = Content {
             entries: vec![
                 Entry::Folder(Folder {
@@ -119,31 +170,6 @@ mod test {
                 }),
             ],
         };
-        let mut output = vec![];
-        let ci = content.iter();
-        for x in &ci {
-            output.push(format!("{}", ci.get_path("/")));
-            if let Entry::File(f) = x {
-                if f.name == "b.txt" {
-                    output.push(format!("b.txt found"));
-                }
-            }
-        }
-        let expected = vec![
-            "a",
-            "a/c.txt",
-            "a/d",
-            "a/d/e.txt",
-            "a/b.txt",
-            "b.txt found",
-            "a/i",
-            "a/f.txt",
-            "g.txt",
-            "h",
-        ];
-        assert_eq!(output.len(), expected.len());
-        for (l, r) in output.iter().zip(expected.iter()) {
-            assert_eq!(l, r);
-        }
+        content
     }
 }
